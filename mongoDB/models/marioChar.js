deb25b68981e99b819be16c9a0b63de2572c2f1c
azim <az1234im@gmail.com>
2021-07-08 13:02:06 +0530

    creating a schema and model

---
+++ mongoDB/models/marioChar.js
@@ -0,0 +1,19 @@
+// this contains mariocharater schema amd model
+
+const mongoose=require('mongoose');
+
+const Schema=mongoose.Schema;
+
+// creating Schema 
+
+const marioCharSchema=new Schema({
+    name:String,
+    weight:Number
+})
+
+// creating model mongoose.model('collection',schema)
+
+const marioChar=mongoose.model('mariochar',mariocharSchema);
+
+// to use it as new marioChar
+module.exports=marioChar;
