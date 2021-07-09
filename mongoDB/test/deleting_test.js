fbcadd000d006a57423c75e0d6090059946592bd
azim <az1234im@gmail.com>
2021-07-09 18:36:34 +0530

    deleting records

---
+++ mongoDB/test/deleting_test.js
@@ -0,0 +1,27 @@
+// for deleting  records .remove
+
+const assert=require('assert');
+const marioChar=require('../models/marioChar')
+
+describe('Deleting the Record',function(){
+    var char;
+    beforeEach(function(done){
+            char=new marioChar({
+                name:"Mario",
+                weight:30
+            });
+            char.save().then(function(){
+                assert(char.isNew===false);
+                done();
+            })
+    })
+// test case
+    it('deletes the record from the database',function(done){
+        marioChar.findOne({name:'lugi'}).then(function(){
+            marioChar.findOne({name:'lugi'}).then(function(result){
+                assert(result===null);
+                done();
+            })
+        })
+    })
+})
--- mongoDB/test/find_test.js
+++ mongoDB/test/find_test.js
@@ -2,8 +2,9 @@ const assert=require('assert')
 const marioChar=require('../models/marioChar')
 
 describe("finding data ",function(){
+    var char;
     beforeEach(function(done){
-        var char=new marioChar({
+         char=new marioChar({
             name:"Mario",
             weight:30
 
@@ -13,9 +14,17 @@ describe("finding data ",function(){
             done();
         })
     });
+    // test 1 
     it('finds one record from database',function(done){
         marioChar.findOne({name:'Mario'}).then(function(result){
             assert(result.name==='Mario');
+            done();
+        })
+    });
+    // test 2 to find by default id 
+    it('finds one record  by default id from database',function(done){
+        marioChar.findOne({_id:char._id}).then(function(result){
+            assert(result._id.toString()===char._id.toString());
             done();
         })
     })
